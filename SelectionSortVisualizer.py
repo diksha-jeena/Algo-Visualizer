f0fcb62616df408808fb42c8e338ea434f7f6bde
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-08-20 21:23:00 +0530

    Sorting an array graphically using Selecting sort

--- SelectionSortVisualizer.py
+++ SelectionSortVisualizer.py
@@ -12,3 +12,16 @@ def selection_sort(arr):
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
         yield arr        
+
+def visualize_selection_sort():
+    arr = np.random.randint(1, 100, 20)
+    fig, ax = plt.subplots()
+    bars = ax.bar(range(len(arr)), arr)
+    for sorted_arr in selection_sort(arr):
+        for bar, height in zip(bars, sorted_arr):
+            bar.set_height(height)
+        plt.pause(0.1) 
+
+    plt.show()
+
+visualize_selection_sort()
