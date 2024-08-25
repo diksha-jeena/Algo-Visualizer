2838076ceacafc3764510f4e0fe2d44f87b1c02e
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-08-25 21:11:20 +0530

    selection sort

--- bubbleSelectionSortVisualizer.py
+++ bubbleSelectionSortVisualizer.py
@@ -10,3 +10,12 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 yield arr
 
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        print(arr)    
