972b351b086eabc00c313ec7aac625de8bd9182c
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-08-25 21:10:32 +0530

    bubbleSort

--- bubbleSelectionSortVisualizer.py
+++ bubbleSelectionSortVisualizer.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import time
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0 , n-i-1):
+            if(arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                yield arr
 
