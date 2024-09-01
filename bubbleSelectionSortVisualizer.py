29170bd1eba608db7b515f6e30e9f0ac58309326
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-09-01 12:41:51 +0530

    visualizing sort

--- bubbleSelectionSortVisualizer.py
+++ bubbleSelectionSortVisualizer.py
@@ -19,3 +19,14 @@ def selection_sort(arr):
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
         print(arr)    
+
+def visualize_selection_sort():
+    arr = np.random.randint(1, 100, 20)
+    fig, ax = plt.subplots()
+    bars = ax.bar(range(len(arr)), arr)
+    for sorted_arr in selection_sort(arr):
+        for bar, height in zip(bars, sorted_arr):
+            bar.set_height(height)
+        plt.pause(0.1)  
+
+    plt.show()
